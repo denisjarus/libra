1903646e3277fdb50d56b1f473c2ca1158a48adf
Denis Jarus <dexterianjarus@gmail.com>
2013-05-23 17:17:17 +0600

    Commit is basically flying off the hook
    It's like commit wants nothing to do with that hook

--- issofat.js
+++ issofat.js
@@ -1,60 +1,83 @@
-var leftPan, rightPan,
+var ui = {},
 	maxWeight = 0,
 	dragOffset = {};
 
 window.onload = function() {
-	var items = document.getElementsByClassName('item');
-	for (var i = 0, len = items.length; i < len; i++) {
-		var item = items[i];
+	ui.arrow = document.getElementById('arrow');
+	ui.proceed = document.getElementById('proceed');
+	ui.leftPan = document.getElementById('left-pan');
+	ui.rightPan = document.getElementById('right-pan');
+
+	//init items
+	var ground = document.getElementById('ground'),
+		bounds = ground.getBoundingClientRect();
+
+	ui.items = document.getElementsByClassName('item');
+
+	for (var i = 0, len = ui.items.length; i < len; i++) {
+		var item = ui.items[i];
 		item.id = 'item' + i;
 		item.addEventListener('dragstart', onDrag, false);
-		item.weight = i + 1;
+		item.addEventListener('dragend', onDragEnd, false);
+		item.weight = 1 + i;
+		item.style.left = Math.random() * (bounds.width - item.getBoundingClientRect().width) + 'px';
+
 		maxWeight += item.weight;
 	}
 
-	leftPan = document.getElementById('left-pan');
-	rightPan = document.getElementById('right-pan');
+	//init targets
+	var targets = [ui.leftPan, ui.rightPan, ground];
 
-	var targets = [leftPan, rightPan, document.getElementById('ground')];
 	for (i = 0, len = targets.length; i < len; i++) {
 		var target = targets[i];
-		target.addEventListener('dragover', onOver, false);
-		target.addEventListener('drop', onDrop, false);
+		target.addEventListener('dragover', onDragOver, false);
+		target.addEventListener('drop', onDrop, true);
 	}
+
+	saveGame(true);
+	update();
 }
 
 function onDrag(event) {
-	event.dataTransfer.setData('Text', event.target.id);
-
-	var position = event.target.getBoundingClientRect();
+	var item = event.target,
+		position = item.getBoundingClientRect();
 
 	dragOffset.x = position.left - event.clientX;
 	dragOffset.y = event.clientY - position.bottom;
 
-	event.target.style.transition = 'bottom 0s';
+	item.style.transition = 'bottom 0s, opacity 0.5s';
+	item.style.opacity = '0.1';
+
+	event.dataTransfer.setData('Text', item.id);
 }
 
-function onOver(event) {
-	if (event.target.className === 'target') {
-		event.preventDefault();
-	}
+function onDragEnd(event) {
+	event.target.style.opacity = null;
+}
+
+function onDragOver(event) {
+	event.preventDefault();
 }
 
 function onDrop(event) {
 	var item = document.getElementById(event.dataTransfer.getData('Text')),
-		itemSize = item.getBoundingClientRect(),
-		position = event.target.getBoundingClientRect();
+		bounds = event.currentTarget.getBoundingClientRect(),
+		x = event.clientX - bounds.left + dragOffset.x,
+		y = bounds.bottom - event.clientY + dragOffset.y;
+
+	if (! item) return;
 
 	event.preventDefault();
-	event.target.appendChild(item);
+	event.currentTarget.appendChild(item);
 
-	item.style.left = event.clientX - position.left + dragOffset.x + 'px';
-	item.style.bottom = (position.bottom - event.clientY) + dragOffset.y + 'px';
+	item.style.left = clamp(x, 0, bounds.width - item.getBoundingClientRect().width) + 'px';
+	item.style.bottom = y + 'px';
 
 	window.setTimeout(function() {
-		item.style.transition = 'bottom 0.15s ease-in';
-		item.style.bottom = 0;
-	}, 10);
+		item.style.transition = null;
+		item.style.bottom = null;
+		item.style.opacity = '1';
+	}, 50);
 
 	update();
 }
@@ -64,11 +87,63 @@ function getWeight(target) {
 	for (var i = 0, len = target.children.length; i < len; i++) {
 		weight += target.children[i].weight;
 	}
-	return 50 + (weight / maxWeight) * 25 + '%';
+	return (weight / maxWeight);
+}
+
+function clamp(value, min, max) {
+	return Math.max(min, Math.min(value, max));
 }
 
 function update() {
-	leftPan.style.height = getWeight(leftPan);
-	rightPan.style.height = getWeight(rightPan);
+	var weight1 = getWeight(ui.leftPan),
+		weight2 = getWeight(ui.rightPan);
+
+	ui.leftPan.style.height = 405 + weight1 * 60 + 'px';
+	ui.rightPan.style.height = 405 + weight2 * 60 + 'px';
+	ui.arrow.style['-webkit-transform'] = 
+	ui.arrow.style.transform ='rotate(' + Math.max(-45, Math.min((weight2 - weight1) * 135, 45)) + 'deg)';
+
+	if (weight1 === 0.5 && weight2 === 0.5) {
+		ui.proceed.style.color = null;
+		ui.proceed.style.textDecoration = null;
+		ui.proceed.setAttribute('href', 'http://www.mspaintadventures.com/?s=6&p=pony');
+	} else {
+		ui.proceed.style.color = 'gray';
+		ui.proceed.style.textDecoration = 'none';
+		ui.proceed.setAttribute('href', '#');
+	}
 }
 
+function saveGame(session) {
+	try {
+		var storage = session ? sessionStorage : localStorage;
+		for (var i = 0, len = ui.items.length; i < len; i++) {
+			var item = ui.items[i];
+
+			storage[i] = JSON.stringify({
+				id: item.id,
+				parentId: item.parentNode.id,
+				position: item.style.left
+			});
+		}
+	} catch(error) {
+		console.log(error);
+	}
+}
+
+function loadGame(session) {
+	try {
+		var storage = session ? sessionStorage : localStorage;
+		for (var i = 0, len = ui.items.length; i < len; i++) {
+			var data = JSON.parse(storage[i]),
+				item = document.getElementById(data.id);
+
+			document.getElementById(data.parentId).appendChild(item);
+			item.style.left = data.position;
+
+			update();
+		}
+	} catch(error) {
+		console.log(error);
+	}
+}
